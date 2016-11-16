b3c535db5508604bc8a6ec5b2bf7ddb9ed55d0c3
XuDeMing <deming.xu@meelive.cn>
2016-11-16 19:52:39 +0800

    modify

--- server/tmp/foot.js
+++
@@ -1,6 +0,0 @@
-var getHtml=require('/Users/inke/Documents/xdm/tirger/server/server/base/render.js').getHtml;
-function _getHtml(obj,_data){
-let html='';
-html+='</body></html>'
-return html;}
-exports._getHtml=_getHtml
