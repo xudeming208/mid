f21eff1fd638d5c187b16e69e6c6670d547d5bc6
XuDeMing <deming.xu@meelive.cn>
2016-12-08 18:32:27 +0800

    update

--- apps/pc/static/js/page/index.js
+++ apps/pc/static/js/page/index.js
@@ -1,3 +1,6 @@
 fml.define("page/index", [], function(require, exports) {
 	console.log(9999)
+	$('#link').on('click',function(){
+		window.location.href='http://pc.fedevot.test.com:8083/'
+	})
 });
