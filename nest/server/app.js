d1dbda3cd35ea783827a681638d8c7d25b9d930f
咸菜 <xudeming208@126.com>
2016-12-19 11:53:08 +0800

    Update app.js

--- nest/server/app.js
+++ nest/server/app.js
@@ -64,3 +64,7 @@ if (cluster.isMaster) {
 		console.log(`the Server has started on ${ip}:${port} at ${new Date().toLocaleString()}`.green.underline);
 	});
 }
+
+process.on('uncaughtException', function (err) {
+    console.log(err)
+})
